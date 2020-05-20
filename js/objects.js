47b41c3576f661005c9e91fd7763fd4495d3da81
Mark Sherman <shermanm@emmanuel.edu>
2020-05-20 15:50:18 -0400

    Convert to 2-space indents

--- js/objects.js
+++ js/objects.js
@@ -5,126 +5,126 @@ Original Date: June 2017
 */
 
 function Cell(i, j, size) {
-	this.x = i * size;
-	this.y = j * size;
+  this.x = i * size;
+  this.y = j * size;
 
-	this.mine = false;
-	this.revealed = false;
-	this.size = size;
-	this.flagged = false;
-	this.neighborMines = -1;
+  this.mine = false;
+  this.revealed = false;
+  this.size = size;
+  this.flagged = false;
+  this.neighborMines = -1;
 
-	this.particles = [];
+  this.particles = [];
 
-	this.show = function() {
-		stroke(0,0,0,0.2);
-		var offset = (this.size / 2);
-		// If this block is revealed,
-		// then if it's a mine show/animate a mine
-		// if its not, show a number or block
-		if(this.revealed) {
-			if(this.mine) {
-				fill(250,105,0, 0.8);
-				rect(this.x, this.y, this.size, this.size);
-				fill(40, 40, 40, 1);
-				ellipse(this.x + offset, this.y + offset, this.size/2);
-			}
-			// NOT a mine
-			else {
-				fill(105,210,231, 0.6);
-				rect(this.x, this.y, this.size, this.size);
+  this.show = function() {
+    stroke(0,0,0,0.2);
+    var offset = (this.size / 2);
+    // If this block is revealed,
+    // then if it's a mine show/animate a mine
+    // if its not, show a number or block
+    if(this.revealed) {
+      if(this.mine) {
+        fill(250,105,0, 0.8);
+        rect(this.x, this.y, this.size, this.size);
+        fill(40, 40, 40, 1);
+        ellipse(this.x + offset, this.y + offset, this.size/2);
+      }
+      // NOT a mine
+      else {
+        fill(105,210,231, 0.6);
+        rect(this.x, this.y, this.size, this.size);
 
-				textSize(this.size);
-				textAlign(CENTER);
-				noStroke();
-				fill(50, 50, 50, 0.7);
-				if(this.neighborMines !== 0) {
-					text(this.neighborMines, this.x + offset, this.y + (offset * 1.75));
-				}
-
-
-			}
-		}
-		// NOT revealed yet
-		else {
-			if(this.flagged) {
-				fill(244,221,81, 0.7);
-				rect(this.x, this.y, this.size, this.size);
-
-				textSize(this.size);
-				textAlign(CENTER);
-				noStroke();
-				fill(50, 50, 50, 0.7);
-				text('🚩', this.x + offset, this.y + (offset * 1.75));
-			} 
-			//not flagged
-			else {
-				fill(105,210,231, 1);
-				rect(this.x, this.y, this.size, this.size);
-			}
-		}
-
-		//Display all particles (if any)
-		if(this.particles.length) {
-			for(let b = this.particles.length - 1; b >= 0; b--) {
-				if(this.particles[b].lifetime > 0) {
-					this.particles[b].show();
-				}
-			}
-		}
-	}
-
-	this.reveal = function() {
-		this.revealed = true;
-	}
-
-	this.setFlagged = function(flag) {
-		this.flagged = flag;
-	}
-
-	this.setNeighborCount = function(c) {
-		this.neighborMines = c;
-	}
+        textSize(this.size);
+        textAlign(CENTER);
+        noStroke();
+        fill(50, 50, 50, 0.7);
+        if(this.neighborMines !== 0) {
+          text(this.neighborMines, this.x + offset, this.y + (offset * 1.75));
+        }
 
 
-	this.explode = function(particles) {
-		this.revealed = true;
-		let s = this.size * 0.50;
+      }
+    }
+    // NOT revealed yet
+    else {
+      if(this.flagged) {
+        fill(244,221,81, 0.7);
+        rect(this.x, this.y, this.size, this.size);
 
-		if(!particles) return;
+        textSize(this.size);
+        textAlign(CENTER);
+        noStroke();
+        fill(50, 50, 50, 0.7);
+        text('🚩', this.x + offset, this.y + (offset * 1.75));
+      } 
+      //not flagged
+      else {
+        fill(105,210,231, 1);
+        rect(this.x, this.y, this.size, this.size);
+      }
+    }
 
-		for(let i = 0; i < 4; i++) {
-			let randVect = createVector(Math.floor(random(-6, 6)), Math.floor(random(-6, 6)));
-			this.particles.push(new Particle(this.x, this.y, randVect, s));
-		}
-	}
+    //Display all particles (if any)
+    if(this.particles.length) {
+      for(let b = this.particles.length - 1; b >= 0; b--) {
+        if(this.particles[b].lifetime > 0) {
+          this.particles[b].show();
+        }
+      }
+    }
+  }
+
+  this.reveal = function() {
+    this.revealed = true;
+  }
+
+  this.setFlagged = function(flag) {
+    this.flagged = flag;
+  }
+
+  this.setNeighborCount = function(c) {
+    this.neighborMines = c;
+  }
+
+
+  this.explode = function(particles) {
+    this.revealed = true;
+    let s = this.size * 0.50;
+
+    if(!particles) return;
+
+    for(let i = 0; i < 4; i++) {
+      let randVect = createVector(Math.floor(random(-6, 6)), Math.floor(random(-6, 6)));
+      this.particles.push(new Particle(this.x, this.y, randVect, s));
+    }
+  }
 }
 
 function Particle(x, y, vel, size) {
-	this.accel = createVector(0, 0.4);
-	this.lifetime = 50;
-	this.pos = createVector(x, y);
-	this.vel = vel;
-	this.size = size;
+  this.accel = createVector(0, 0.4);
+  this.lifetime = 50;
+  this.pos = createVector(x, y);
+  this.vel = vel;
+  this.size = size;
 
-	this.show = () => {
-		this.lifetime += (this.lifetime > 0) ? -1 : 0;
-		if(this.lifetime > 0) {
-			this.lifetime--;
+  this.show = () => {
+    this.lifetime += (this.lifetime > 0) ? -1 : 0;
+    if(this.lifetime > 0) {
+      this.lifetime--;
 
-			//Update position based on velocity
-			this.pos.add(this.vel);
+      //Update position based on velocity
+      this.pos.add(this.vel);
 
-			//Update velocity based on acceleration
-			this.vel.add(this.accel);
+      //Update velocity based on acceleration
+      this.vel.add(this.accel);
 
-			//Add some drag
-			// this.vel.x += 2;
+      //Add some drag
+      // this.vel.x += 2;
 
-			fill(35, 35, 35, this.lifetime / 100);
-			noStroke();
-			ellipse(this.pos.x, this.pos.y, this.size);
+      fill(35, 35, 35, this.lifetime / 100);
+      noStroke();
+      ellipse(this.pos.x, this.pos.y, this.size);
 
-		}
-	}
+    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -30,38 +30,38 @@ const PLAYING = 0, WON = 1, LOST = 2;
 
 //This method runs once per DOM load
 function setup() {
-	colorMode(RGB, 255, 255, 255, 1);
+  colorMode(RGB, 255, 255, 255, 1);
 
-	let w = Math.floor(window.innerHeight * 0.8);
-	CANVAS_W = CANVAS_H = w < 801 ? w : 801;
+  let w = Math.floor(window.innerHeight * 0.8);
+  CANVAS_W = CANVAS_H = w < 801 ? w : 801;
 
-	//Reset the game
-	userInterface();
-	resetGame();
-		
-	//Create canvas
-	myCanvas = createCanvas(CANVAS_W, CANVAS_H);
-	myCanvas.parent('canvas-holder');
+  //Reset the game
+  userInterface();
+  resetGame();
+    
+  //Create canvas
+  myCanvas = createCanvas(CANVAS_W, CANVAS_H);
+  myCanvas.parent('canvas-holder');
 
 }
 
 //Runs as much as 60 times a second.
 function draw() {
-	//Sets background color
-	background(255, 255, 255, 1);
+  //Sets background color
+  background(255, 255, 255, 1);
 
-	//Shows all cells in the grid
-	for(let i = 0; i < GRID_SIZE; i++) {
-		for(let j = 0; j < GRID_SIZE; j++) {
-			grid[i][j].show();
-		}
-	}
+  //Shows all cells in the grid
+  for(let i = 0; i < GRID_SIZE; i++) {
+    for(let j = 0; j < GRID_SIZE; j++) {
+      grid[i][j].show();
+    }
+  }
 
-	//End game can be "won" or "lost"
-	//Depending on score
-	if(state !== PLAYING) {
-		displayEndGame();
-	}
+  //End game can be "won" or "lost"
+  //Depending on score
+  if(state !== PLAYING) {
+    displayEndGame();
+  }
   
   if(state === PLAYING) {
     updateCursorPos();
@@ -70,104 +70,104 @@ function draw() {
 
 //Sets values related to the DOM and creates events
 function userInterface() {
-	particles      = document.getElementById('particles');
-	diff           = document.getElementById('difficulty');
-	restartBtn     = document.getElementById('restart');
-	scoreHolder    = document.getElementById('score');
+  particles      = document.getElementById('particles');
+  diff           = document.getElementById('difficulty');
+  restartBtn     = document.getElementById('restart');
+  scoreHolder    = document.getElementById('score');
 
-	restartBtn.addEventListener('click', resetGame);
-	diff.addEventListener('change', resetGame);
+  restartBtn.addEventListener('click', resetGame);
+  diff.addEventListener('change', resetGame);
 }
 
 function mouseReleased() { //Is called every time a mouse button is released
 
-	//Position of mouse when released
-	let i = Math.floor(mouseX / CELL_SIZE);
-	let j = Math.floor(mouseY / CELL_SIZE);
+  //Position of mouse when released
+  let i = Math.floor(mouseX / CELL_SIZE);
+  let j = Math.floor(mouseY / CELL_SIZE);
 
-	//Was the mouse released outside of the grid?
-	//Are we not currently playing the game?
-	if(i > GRID_SIZE-1 || j > GRID_SIZE-1 || i < 0 || j < 0 || (state !== PLAYING)) return;
+  //Was the mouse released outside of the grid?
+  //Are we not currently playing the game?
+  if(i > GRID_SIZE-1 || j > GRID_SIZE-1 || i < 0 || j < 0 || (state !== PLAYING)) return;
 
-	switch(mouseButton) {
-		case LEFT:
-			touchCell(i, j);
-		break;
+  switch(mouseButton) {
+    case LEFT:
+      touchCell(i, j);
+    break;
 
-		case RIGHT:
-			flagCell(i, j);
-		break;
-	}
+    case RIGHT:
+      flagCell(i, j);
+    break;
+  }
 }
 
 function touchCell(i, j) {
-	//If the cell is not flagged
-	//And is a mine, Game over.
-	if(!grid[i][j].flagged) {
-		if(grid[i][j].mine) {
-			for(let s = 0; s < mines.length; s++) {
-				grid[mines[s][0]][mines[s][1]].explode(particles.checked);
-			}
-			gameOver();
-		} 
-		else { //Show the cell contents
-			revealCell(i, j);
-		}
-	}
+  //If the cell is not flagged
+  //And is a mine, Game over.
+  if(!grid[i][j].flagged) {
+    if(grid[i][j].mine) {
+      for(let s = 0; s < mines.length; s++) {
+        grid[mines[s][0]][mines[s][1]].explode(particles.checked);
+      }
+      gameOver();
+    } 
+    else { //Show the cell contents
+      revealCell(i, j);
+    }
+  }
 }
 
 function flagCell(i, j) {
-	//If flagged, remove it.
-	if(grid[i][j].flagged) {
-		grid[i][j].setFlagged(false);
-		flags--;
-	} 
-	//Cannot let the user create more flags than there are mines
-	else if(flags < MINE_COUNT) {
-		grid[i][j].setFlagged(true);
-		
-		//No need to do checkWin() if flags does not equal mineCount.
-		if(++flags != MINE_COUNT) {
-			return;
-		}
-	}
+  //If flagged, remove it.
+  if(grid[i][j].flagged) {
+    grid[i][j].setFlagged(false);
+    flags--;
+  } 
+  //Cannot let the user create more flags than there are mines
+  else if(flags < MINE_COUNT) {
+    grid[i][j].setFlagged(true);
+    
+    //No need to do checkWin() if flags does not equal mineCount.
+    if(++flags != MINE_COUNT) {
+      return;
+    }
+  }
 
-	checkWin();
+  checkWin();
 }
 
 function resetGame() {
-	mines = [];
-	grid = [];
-	GRID_SIZE = diff.value;
+  mines = [];
+  grid = [];
+  GRID_SIZE = diff.value;
 
-	document.getElementById('ui-holder').style.width = CANVAS_W + 'px';	
-	
-	//Minimum = 10% of the board are mines
-	//Maximum = 14% of the board are mines
-	MIN_MINES = 0.10 * GRID_SIZE * GRID_SIZE;
-	MAX_MINES = 0.14 * GRID_SIZE * GRID_SIZE;
+  document.getElementById('ui-holder').style.width = CANVAS_W + 'px';	
+  
+  //Minimum = 10% of the board are mines
+  //Maximum = 14% of the board are mines
+  MIN_MINES = 0.10 * GRID_SIZE * GRID_SIZE;
+  MAX_MINES = 0.14 * GRID_SIZE * GRID_SIZE;
 
-	//Random number of mines
-	MINE_COUNT = Math.floor(random(MIN_MINES, MAX_MINES));
+  //Random number of mines
+  MINE_COUNT = Math.floor(random(MIN_MINES, MAX_MINES));
 
-	//Cell size in pixels.
-	CELL_SIZE = CANVAS_H / GRID_SIZE;
+  //Cell size in pixels.
+  CELL_SIZE = CANVAS_H / GRID_SIZE;
 
-	//stores the cells in a grid array
-	//used for iterating through all the cells
-	for(let i = 0; i < GRID_SIZE; i++) {
-		let temp = []
-		for(let j = 0; j < GRID_SIZE; j++) {
-			temp.push(new Cell(i, j, CELL_SIZE));
-		}
-		grid.push(temp);
-	}
+  //stores the cells in a grid array
+  //used for iterating through all the cells
+  for(let i = 0; i < GRID_SIZE; i++) {
+    let temp = []
+    for(let j = 0; j < GRID_SIZE; j++) {
+      temp.push(new Cell(i, j, CELL_SIZE));
+    }
+    grid.push(temp);
+  }
 
-	//Set the mines, playing game, and reset variables
-	setMines(MINE_COUNT);
-	state = PLAYING;
-	flags = 0;
-	updateScore(0);
+  //Set the mines, playing game, and reset variables
+  setMines(MINE_COUNT);
+  state = PLAYING;
+  flags = 0;
+  updateScore(0);
 
 }
 
@@ -176,114 +176,114 @@ function resetGame() {
 
 // Sets count amount of mines in the global grid
 function setMines(count) {
-	while(count) {
-		let i = Math.floor(random(GRID_SIZE));
-		let j = Math.floor(random(GRID_SIZE));
+  while(count) {
+    let i = Math.floor(random(GRID_SIZE));
+    let j = Math.floor(random(GRID_SIZE));
 
-		if(grid[i][j].mine === false) {
-			mines.push([i, j]);
-			grid[i][j].mine = true;
-			count--;
-		}
-	}
+    if(grid[i][j].mine === false) {
+      mines.push([i, j]);
+      grid[i][j].mine = true;
+      count--;
+    }
+  }
 
-	//Set the neighbor counts on the grid.
-	for(let i = 0; i < GRID_SIZE; i++) {
-		for(let j = 0; j < GRID_SIZE; j++) {
-			if(!grid[i][j].mine) {
-				grid[i][j].setNeighborCount(thisManyMines(getNeighbors(i, j)));
-			}
-		}
-	}
+  //Set the neighbor counts on the grid.
+  for(let i = 0; i < GRID_SIZE; i++) {
+    for(let j = 0; j < GRID_SIZE; j++) {
+      if(!grid[i][j].mine) {
+        grid[i][j].setNeighborCount(thisManyMines(getNeighbors(i, j)));
+      }
+    }
+  }
 }
 
 //Returns number of mines in the input list
 function thisManyMines(list) {
-	var count = 0;
-	for(let x = 0; x < list.length; x++) {
-		count += grid[list[x][0]][list[x][1]].mine ? 1 : 0;
-	}
-	return count;
+  var count = 0;
+  for(let x = 0; x < list.length; x++) {
+    count += grid[list[x][0]][list[x][1]].mine ? 1 : 0;
+  }
+  return count;
 }
 
 //Returns an array of objects that contains
 //the neighbors of that coordinate pair
 function getNeighbors(i, j) {
-	var neighbors = [];
+  var neighbors = [];
 
-	for(let k = i - 1; k < i + 2; k++) {
-		for(let l = j - 1; l < j + 2; l++) {
-			if(k >= 0 && l >= 0 && l < GRID_SIZE && k < GRID_SIZE && !(k === i && l === j)) {
-				neighbors.push([k, l]);
-			}
-		}
-	}
+  for(let k = i - 1; k < i + 2; k++) {
+    for(let l = j - 1; l < j + 2; l++) {
+      if(k >= 0 && l >= 0 && l < GRID_SIZE && k < GRID_SIZE && !(k === i && l === j)) {
+        neighbors.push([k, l]);
+      }
+    }
+  }
 
-	return neighbors;
+  return neighbors;
 }
 
 //Is called when a cell is clicked and is not flagged.
 //RECURSIVE CALLS
 function revealCell(i, j) {
-	if(!grid[i][j].flagged) {
-		grid[i][j].reveal();
-	}
+  if(!grid[i][j].flagged) {
+    grid[i][j].reveal();
+  }
 
-	//If the neighbor has 0 mines, reveal it.
-	if(grid[i][j].neighborMines === 0) {
-		let myNeighbors = getNeighbors(i, j);
-		for(let s = 0; s < myNeighbors.length; s++) {
-			if(!grid[myNeighbors[s][0]][myNeighbors[s][1]].revealed) {
-				revealCell(myNeighbors[s][0], myNeighbors[s][1]);
-			}
-		}
-	}
+  //If the neighbor has 0 mines, reveal it.
+  if(grid[i][j].neighborMines === 0) {
+    let myNeighbors = getNeighbors(i, j);
+    for(let s = 0; s < myNeighbors.length; s++) {
+      if(!grid[myNeighbors[s][0]][myNeighbors[s][1]].revealed) {
+        revealCell(myNeighbors[s][0], myNeighbors[s][1]);
+      }
+    }
+  }
 }
 
 //Set game state lost
 function gameOver() {
-	updateScore(score());
-	state = LOST;
+  updateScore(score());
+  state = LOST;
 }
 
 //Set game state to win
 function gameWon() {
-	updateScore(score());
-	state = WON;
+  updateScore(score());
+  state = WON;
 }
 
 //DOM manipulation based on game state
 function displayEndGame() {
-	fill(240,240,240,0.8);
-	noStroke();
-	rect(0, CANVAS_H/4, CANVAS_W, CANVAS_H/2);
+  fill(240,240,240,0.8);
+  noStroke();
+  rect(0, CANVAS_H/4, CANVAS_W, CANVAS_H/2);
 
-	textSize(CANVAS_W/8);
-	textAlign(CENTER);
-	noStroke();
-	fill(50, 50, 50, 0.7);
+  textSize(CANVAS_W/8);
+  textAlign(CENTER);
+  noStroke();
+  fill(50, 50, 50, 0.7);
 
-	let msg;
-	if(state === WON) {
-		fill(0,142,9, 1);
-		msg = 'You Won!';
-	} 
-	else {
-		fill(255,3,3, 1);
-		msg = 'You Lost!';
-	}
+  let msg;
+  if(state === WON) {
+    fill(0,142,9, 1);
+    msg = 'You Won!';
+  } 
+  else {
+    fill(255,3,3, 1);
+    msg = 'You Lost!';
+  }
 
-	text(msg, CANVAS_W/2, CANVAS_H/2);
+  text(msg, CANVAS_W/2, CANVAS_H/2);
 
-	textSize(CANVAS_W/16);
-	msg = 'SCORE: ' + score() + ' / 100';
-	text(msg, CANVAS_W/2, CANVAS_H/2 + 80);
+  textSize(CANVAS_W/16);
+  msg = 'SCORE: ' + score() + ' / 100';
+  text(msg, CANVAS_W/2, CANVAS_H/2 + 80);
 
 }
 
 //Update DOM element with score (once game is over)
 function updateScore(s) {
-	scoreHolder.innerHTML = 'Score: ' + s + ' / 100';
+  scoreHolder.innerHTML = 'Score: ' + s + ' / 100';
 }
 
 //Borrow the score DOM element to show the current grid postion under cursor
@@ -299,19 +299,19 @@ function updateCursorPos() {
 
 //Are all the mines flagged?
 function checkWin() {
-	if(minesFlagged() === mines.length) gameWon();
+  if(minesFlagged() === mines.length) gameWon();
 }
 
 //Goes through each cell object that is a mine
 //And returns the number of them that are flagged.
 function minesFlagged() {
-	let count = 0;
-	for(let i = 0; i < mines.length; i++) {
-		if( grid[mines[i][0]][mines[i][1]].flagged ) {
-			count++;
-		}
-	}
-	return count;
+  let count = 0;
+  for(let i = 0; i < mines.length; i++) {
+    if( grid[mines[i][0]][mines[i][1]].flagged ) {
+      count++;
+    }
+  }
+  return count;
 }
 
 //Returns the score in a percentage format.
